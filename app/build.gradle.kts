2e04eafff86abdab1faed07780f65f2f8d2fea9a
bperezll <borja.perez.llano@gmail.com>
2024-02-29 11:47:55 +0100

    SQLite implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
 
 dependencies {
 
+    // SQLite implementation
+    val sqliteVersion = "2.4.0"
+    implementation("androidx.sqlite:sqlite-ktx:$sqliteVersion")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
