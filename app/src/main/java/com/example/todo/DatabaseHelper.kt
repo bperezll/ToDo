42f5c62fc318389edabd39d4768bb0adf524b1e2
bperezll <borja.perez.llano@gmail.com>
2024-02-29 13:43:03 +0100

    Adding example tasks to test db functionality

--- app/src/main/java/com/example/todo/DatabaseHelper.kt
+++ app/src/main/java/com/example/todo/DatabaseHelper.kt
@@ -1,8 +1,10 @@
 package com.example.todo
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 class DatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
@@ -30,5 +32,42 @@ class DatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_NAME,
 
     private fun destroyDatabase(db: SQLiteDatabase) {
         db.execSQL(SQL_DELETE_TABLE)
+    }
+
+    fun createTask() {
+        val db = writableDatabase
+        // db.execSQL("INSERT INTO Task(task, done) VALUES ('Comprar leche', false)")
+
+        var values = ContentValues()
+        values.put("task", "Comprar leche")
+        values.put("done", false)
+
+        val newRowId = db.insert("Task", null, values)
+        Log.i("databasew", "New record ID: $newRowId")
+
+        values = ContentValues()
+        values.put("task", "Limpiar el coche")
+        values.put("done", false)
+    }
+
+    fun readTasks() {
+        val db= writableDatabase
+        val cursor = db.query(
+            "Task",
+            arrayOf("id", "task", "done"),
+            null,
+            null,
+            null,
+            null,
+            "done DESC"
+        )
+        if(cursor.moveToFirst()) {
+            do {
+                val id = cursor.getString(0)
+                val task = cursor.getString(1)
+                val done = cursor.getInt(2) == 1
+                Log.i("DATABASE", "$id -> Task: $task, Done: $done")
+            } while (cursor.moveToNext())
+        }
     }
 }
